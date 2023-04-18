9149b44b9c063e0f13969cd0bf6b8865bc172b6b
Duarte OC <duarteocarmo@gmail.com>
2023-04-18 14:20:50 +0200

    almost done

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder, Error};
+use actix_web::middleware::Logger;
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 mod process;
-use tera::{ Tera, Context };
+use cronjob::CronJob;
+use env_logger::Env;
 use std::fs::File;
 use std::io::Read;
+use tera::{Context, Tera};
 
 #[get("/")]
 async fn hello(templ: web::Data<Tera>) -> impl Responder {
@@ -14,19 +17,16 @@ async fn hello(templ: web::Data<Tera>) -> impl Responder {
     HttpResponse::Ok().body(s)
 }
 
-#[get("/episodes/{episode_id}")] 
+#[get("/episodes/{episode_id}")]
 async fn episode_pages(path: web::Path<i32>, templ: web::Data<Tera>) -> impl Responder {
-
     let episode_id = path.into_inner();
     let episodes = process::get_episodes().await;
     let episode = episodes.iter().find(|e| e.number == episode_id).unwrap();
 
-
     let mut file = File::open(&episode.transcript_location).expect("Failed to open file");
     let mut transcript = String::new();
-    file.read_to_string(&mut transcript).expect("Failed to read file");
-
-
+    file.read_to_string(&mut transcript)
+        .expect("Failed to read file");
 
     let mut context = Context::new();
     context.insert("episode", episode);
@@ -36,14 +36,29 @@ async fn episode_pages(path: web::Path<i32>, templ: web::Data<Tera>) -> impl Res
     HttpResponse::Ok().body(s)
 }
 
+fn on_cron(name: &str) {
+    println!("{}: Updating episodes!", name);
+    process::main();
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Updater
+    let mut cron = CronJob::new("Test Cron", on_cron);
+    cron.minutes("11");
+    cron.seconds("30");
+    cron.offset(0);
+    CronJob::start_job_threaded(cron);
+
+    // Log requests
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
 
     HttpServer::new(|| {
         let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*")).unwrap();
 
         App::new()
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
             .app_data(web::Data::new(tera))
             .service(hello)
             .service(episode_pages)
@@ -52,4 +67,3 @@ async fn main() -> std::io::Result<()> {
     .run()
     .await
 }
-
--- src/process.rs
+++ src/process.rs
@@ -1,22 +1,23 @@
-use feed_rs::{parser, model::Feed};
-use hound;
-use std::io::Cursor;
-use std::fs::File;
-use std::io::Write;
-use whisper_rs::{FullParams, SamplingStrategy, WhisperContext};
-use futures::{stream, StreamExt}; 
-use reqwest::Client; 
 use chrono::{DateTime, Utc};
-use tokio; 
+use feed_rs::{model::Feed, parser};
+use futures::{stream, StreamExt};
+use hound;
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::Cursor;
+use std::io::Write;
 use std::process::Command;
-use serde::{Serialize, Deserialize};
+use tokio;
+use whisper_rs::{FullParams, SamplingStrategy, WhisperContext};
 
+#[allow(dead_code)]
 const CONCURRENT_REQUESTS: usize = 30;
 const RSS_FEED_LOCATION: &str = "https://www.omnycontent.com/d/playlist/8c0a4104-a688-4e57-91fd-ad7b00d5dddd/c2325e96-d6ad-4206-b72b-ad8e00e5f4fe/bbc8a8c5-8da7-46ef-843f-ad8e00e5f515/podcast.rss";
 const AUDIO_FILE_FOLDER: &str = "episodes/";
 const TRANSCRIPT_FOLDER: &str = "transcripts/";
 
-#[derive (Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Episode {
     pub url: String,
     pub title: String,
@@ -34,9 +35,33 @@ async fn fetch_rss_feed() -> Result<Feed, Box<dyn std::error::Error>> {
     return Ok(feed);
 }
 
+async fn download_episode(episode: &Episode) {
+    let client = Client::new();
+    let response = client.get(&episode.url).send().await;
+    let bytes = response.unwrap().bytes().await.unwrap();
 
-#[tokio::main]
-async fn download_episodes_from(list_of_episodes: &Vec<Episode>)  {
+    let mp3_file = episode.file_location.replace(".wav", ".mp3");
+    let mut file = std::fs::File::create(&mp3_file).unwrap();
+    let mut content = Cursor::new(bytes);
+    std::io::copy(&mut content, &mut file).unwrap();
+    println!("Downloaded {}", mp3_file);
+
+    let _output = Command::new("ffmpeg")
+        .arg("-i")
+        .arg(&mp3_file)
+        .arg("-ar")
+        .arg("16000")
+        .arg(&episode.file_location)
+        .output()
+        .expect("failed to execute process");
+    println!("Converted into {}", episode.file_location);
+
+    std::fs::remove_file(&mp3_file).unwrap();
+    println!("Removed {}", mp3_file);
+}
+
+#[allow(dead_code)]
+async fn download_episodes_from(list_of_episodes: &Vec<Episode>) {
     let client = Client::new();
 
     let episodes_to_download = list_of_episodes
@@ -45,7 +70,11 @@ async fn download_episodes_from(list_of_episodes: &Vec<Episode>)  {
         .cloned()
         .collect::<Vec<Episode>>();
 
-    println!("{} episodes to download from {} total.", episodes_to_download.len(), list_of_episodes.len());
+    println!(
+        "{} episodes to download from {} total.",
+        episodes_to_download.len(),
+        list_of_episodes.len()
+    );
 
     let bodies = stream::iter(episodes_to_download)
         .map(|episode| {
@@ -54,19 +83,17 @@ async fn download_episodes_from(list_of_episodes: &Vec<Episode>)  {
             async move {
                 let response = client.get(&episode.url).send().await?;
                 response.bytes().await.map(|bytes| (episode, bytes))
-
             }
         })
         .buffer_unordered(CONCURRENT_REQUESTS);
 
-     bodies
+    bodies
         .for_each(|result| async move {
-            match result  {
+            match result {
                 Ok((episode, bytes)) => {
-
                     let mp3_file = episode.file_location.replace(".wav", ".mp3");
                     let mut file = std::fs::File::create(&mp3_file).unwrap();
-                    let mut content =  Cursor::new(bytes);
+                    let mut content = Cursor::new(bytes);
                     std::io::copy(&mut content, &mut file).unwrap();
                     println!("Downloaded {}", mp3_file);
 
@@ -82,18 +109,14 @@ async fn download_episodes_from(list_of_episodes: &Vec<Episode>)  {
 
                     std::fs::remove_file(&mp3_file).unwrap();
                     println!("Removed {}", mp3_file);
-
                 }
                 Err(e) => {
                     eprint!("Error: {}", e);
-
                 }
             }
         })
-    .await;
-
+        .await;
 }
-
 
 fn format_time(seconds: i64) -> String {
     let seconds = seconds as f32;
@@ -104,26 +127,26 @@ fn format_time(seconds: i64) -> String {
         "{:02}:{:02}:{:02}",
         hours as u32, minutes as u32, seconds as u32
     )
-} 
-
+}
 
 /// Loads a context and model, processes an audio file, and prints the resulting transcript to stdout.
 fn get_transcript(episode: &Episode) -> Result<(), &'static str> {
-
     if std::path::Path::new(&episode.transcript_location).exists() {
-        println!("Transcript already exists for {}", episode.transcript_location);
+        println!(
+            "Transcript already exists for {}",
+            episode.transcript_location
+        );
         return Ok(());
     }
     // Load a context and model.
     let mut ctx = WhisperContext::new("ggml-base.bin").expect("failed to load model");
-
 
     // Create a params object for running the model.
     // Currently, only the Greedy sampling strategy is implemented, with BeamSearch as a WIP.
     // The number of past samples to consider defaults to 0.
     let mut params = FullParams::new(SamplingStrategy::Greedy { best_of: 0 });
 
-    params.set_n_threads(2);
+    params.set_n_threads(4);
     params.set_language(Some("pt"));
     params.set_print_special(false);
     params.set_print_progress(true);
@@ -183,8 +206,6 @@ fn get_transcript(episode: &Episode) -> Result<(), &'static str> {
             start_timestamp_formatted, end_timestamp_formatted, segment
         );
 
-        println!("{}", formatted_string.replace("\n", " "));
-
         file.write_all(formatted_string.as_bytes())
             .expect("failed to write to file");
     }
@@ -197,13 +218,21 @@ pub async fn get_episodes() -> Vec<Episode> {
     let mut list_of_episodes = Vec::new();
 
     for entry in rss_feed.entries.clone() {
-
         let media_content = entry.media.first().unwrap().content.first().unwrap();
         let url = media_content.url.clone().unwrap();
         let url_string = url.as_str();
         let title = entry.title.unwrap().content;
         let date = entry.published.unwrap();
-        let thumbnail_url = entry.media.first().unwrap().thumbnails.first().unwrap().image.uri.clone();
+        let thumbnail_url = entry
+            .media
+            .first()
+            .unwrap()
+            .thumbnails
+            .first()
+            .unwrap()
+            .image
+            .uri
+            .clone();
 
         let episode = Episode {
             url: url_string.to_string(),
@@ -214,7 +243,7 @@ pub async fn get_episodes() -> Vec<Episode> {
             date,
             number: 0,
         };
-    
+
         list_of_episodes.push(episode);
         list_of_episode_urls.push(url_string.to_string());
     }
@@ -231,61 +260,28 @@ pub async fn get_episodes() -> Vec<Episode> {
     }
 
     list_of_episodes
-
-
 }
 
-pub async fn main()  {
-    let rss_feed = fetch_rss_feed().await.unwrap();
+#[tokio::main]
+pub async fn main() {
+    let episodes = get_episodes().await;
+    println!("Number of episodes in feed: {}", episodes.len());
 
-    
-    let mut list_of_episode_urls = Vec::new();
-    let mut list_of_episodes = Vec::new();
-
-    for entry in rss_feed.entries.clone() {
-        let media_content = entry.media.first().unwrap().content.first().unwrap();
-        let url = media_content.url.clone().unwrap();
-        let url_string = url.as_str();
-        let title = entry.title.unwrap().content;
-        let date = entry.published.unwrap();
-
-
-        let episode = Episode {
-            url: url_string.to_string(),
-            title: title.to_string(),
-            file_location: "".to_string(),
-            transcript_location: "".to_string(),
-            thumbnail_url: "".to_string(),
-            date,
-            number: 0,
+    for episode in episodes.iter() {
+        if std::path::Path::new(&episode.transcript_location).exists() {
+            continue;
         };
-    
-        list_of_episodes.push(episode);
-        list_of_episode_urls.push(url_string.to_string());
-    }
+        println!("Getting transcript for episode {}", episode.number);
 
-    list_of_episodes.sort_by_key(|episode| episode.date);
+        // download
+        download_episode(&episode).await;
 
-    for (i, episode) in list_of_episodes.iter_mut().enumerate() {
-        episode.number = i as i32 + 1;
-        let episode_file_name = format!("{:03}", episode.number) + ".wav";
-        let episode_transcript_name = format!("{:03}", episode.number) + ".txt";
-
-        episode.file_location = AUDIO_FILE_FOLDER.to_owned() + &episode_file_name;
-        episode.transcript_location = TRANSCRIPT_FOLDER.to_owned() + &episode_transcript_name;
-    }
-
-    let total_episodes = list_of_episodes.len();
-    println!("Total episodes: {}", total_episodes);
-
-    let limit = 60;
-    download_episodes_from(&list_of_episodes[0..limit].to_vec());
-
-    let second_episode = &list_of_episodes[1];
-    println!("Second episode: {:?}", second_episode);
-
-    for episode in list_of_episodes[0..limit].iter() {
-        println!("Episode: {:?}-{:?}", episode.number, episode.title);
+        // transcribe
         let _parsing = get_transcript(episode);
+        println!("Got transcript in {}", episode.transcript_location);
+
+        // delete
+        std::fs::remove_file(&episode.file_location).expect("failed to delete file");
+        println!("Deleted file {}", episode.file_location);
     }
 }
